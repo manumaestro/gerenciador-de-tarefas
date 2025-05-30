8b4400312cb52cc5ee8ca5bb5284d693ec5742a2
manumaestro <manuela.maestro@aluno.senai.br>
2025-05-30 08:41:46 -0300

    feat: usa o método push e edita o arquivo README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 // Resolução de atividades utilizando os métodos de array
+
+// Primeira etapa
+
+## Usando o método push e adicionando novas tarefas
---
+++ metodos-array/tarefas.js
@@ -0,0 +1,11 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+console.log("Antes do push");
+console.table(tarefas);
+
+// Adicionando uma nova tarefa 
+
+tarefas.push("Jogar the last of us", "Fazer a janta");
+
+console.log("Depois do push");
+console.table(tarefas);
